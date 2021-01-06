b40f244de74a4970a4c824ac5c72c0cc5b522b64
“KiczuPL” <KiczuPL@outloo.com>
2021-01-06 17:41:00 +0100

    Obsługa definicji

---
+++ v1.2/main.c
@@ -0,0 +1,20 @@
+#include "parser.h"
+#include "store.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv) {
+  char *inpname;
+  int i;
+  for (i = 0; i < argc - 1; i++) {
+    inpname = argv[i + 1];
+    analizatorSkladni(inpname);
+  }
+  printf("-----------------------\n");
+  printf("Funkcje w podanych plikach %s: \n", inpname);
+  store_print();
+  printf("-----------------------\n");
+  store_clear();
+  return 0;
+}
